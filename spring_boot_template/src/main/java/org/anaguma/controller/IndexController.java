d267601ba35e9d7dec5f93c7f5731c4fa32bb80b
shoota <shoota.dev@gmail.com>
2015-04-14 16:23:54 +0900

    thymeleaf sample

--- spring_boot_template/src/main/java/org/anaguma/controller/IndexController.java
+++ spring_boot_template/src/main/java/org/anaguma/controller/IndexController.java
@@ -1,8 +1,11 @@
 package org.anaguma.controller;
 
+import org.anaguma.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @ComponentScan
 public class IndexController {
 
+    @Autowired
+    HelloService helloservice;
+
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("message",helloservice.getHelloMessage());
         return "index";
     }
 }
