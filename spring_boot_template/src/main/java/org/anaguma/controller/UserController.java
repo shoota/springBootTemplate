3ca4fa71b258ed2262158598e04c2dc16ad3bdcb
shoota <shoota.dev@gmail.com>
2015-04-14 15:28:11 +0900

    add user controller

---
+++ spring_boot_template/src/main/java/org/anaguma/controller/UserController.java
@@ -0,0 +1,22 @@
+package org.anaguma.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+@EnableAutoConfiguration
+@ComponentScan
+public class UserController {
+
+	@RequestMapping("/user")
+	@ResponseBody
+	public String home() {
+		return "user!";
+	}
+
+}
